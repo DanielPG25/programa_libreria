618f9ec2df82da5c810c9eb4d8d6cd0d06084acc
Daniel Parrales García <dannyparrales16@gmail.com>
2021-04-19 12:35:03 +0200

    Añadida la página principal

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, abort
 import json
 import sys
+import os
 app = Flask(__name__)
 
 with open('./books.json') as f:
 	libreria=json.load(f)
-return(datos)
+
 
 @app.route('/',methods=["GET","POST"])
 def inicio():
-	return render_template("index.html")
+	return render_template("index.html",libreria=libreria)
+
+
+
+
+port=os.environ["PORT"]
+app.run('0.0.0.0',int(port), debug=True)
 	
